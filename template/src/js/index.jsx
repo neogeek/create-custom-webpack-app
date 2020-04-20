d99e45037d4fe6e56957e9fc4cab1ac561ec3835
Scott Doxey <neogeek0101@gmail.com>
2020-04-19 23:43:39 -0400

    Updated template.

--- template/src/js/index.jsx
+++ template/src/js/index.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class HelloMessage extends React.Component {
-    render() {
-        return <div>Hello {this.props.name}</div>;
-    }
-}
-
+const HelloMessage = ({ name }) => {
+    return <div>Hello {name}</div>;
+};
 ReactDOM.render(
     <HelloMessage name="Taylor" />,
-    document.getElementById('hello-example')
+    document.querySelector('#root')
 );
