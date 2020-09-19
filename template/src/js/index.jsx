09f466375399165fd7b0d796b8d430ee698c9d0f
Scott Doxey <neogeek0101@gmail.com>
2020-09-19 19:53:39 -0400

    Added example of scss file import.

--- template/src/js/index.jsx
+++ template/src/js/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import '../css/styles';
+
 import Message from './message';
 
 ReactDOM.render(<Message name="Taylor" />, document.querySelector('#root'));
